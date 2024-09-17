fbaf18d0882e86be1882176669eab63a9bfe2359
Parth Mandloi <parth.mandloi@karkinos.in>
2024-09-17 19:15:08 +0530

    new coomit added

--- src/Components/NetflixSeries.jsx
+++ src/Components/NetflixSeries.jsx
@@ -1,6 +1,7 @@
 import seriesData from "../api/thapa_series.json";
 import { SeriesCard } from "./SeriesCard";
 export const NetflixSeries = () => {
+  console.log("ll");
   return (
     <ul className ="grid grid-three-cols">
       {seriesData.map((curEle) => (<SeriesCard key = {curEle.id} data = {curEle} id = {curEle.id}/>))}
