54b03545efd26d74fd139eadfde6cb1864d23b82
Parth Mandloi <parth.mandloi@karkinos.in>
2024-09-17 20:15:28 +0530

    By parth

--- src/Components/NetflixSeries.jsx
+++ src/Components/NetflixSeries.jsx
@@ -1,7 +1,7 @@
 import seriesData from "../api/thapa_series.json";
 import { SeriesCard } from "./SeriesCard";
 export const NetflixSeries = () => {
-  console.log("ll4448888888");
+  console.log("ll44");
   return (
     <ul className ="grid grid-three-cols">
       {seriesData.map((curEle) => (<SeriesCard key = {curEle.id} data = {curEle} id = {curEle.id}/>))}
